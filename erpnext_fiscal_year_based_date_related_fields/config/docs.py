371ad741c36bbb63b168c62da1f2d873739ec748
Amin Ahmed <kid_1194@hotmail.com>
2022-07-13 18:23:31 +0300

    Create docs.py

---
+++ erpnext_fiscal_year_based_date_related_fields/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/erpnext_fiscal_year_based_date_related_fields"
+# docs_base_url = "https://[org_name].github.io/erpnext_fiscal_year_based_date_related_fields"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "ERPNext Fiscal Year Based Date Related Fields"
