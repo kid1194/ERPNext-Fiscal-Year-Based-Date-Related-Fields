d9d617784f869aac14d87903b75cbcffc9226b25
Amin Ahmed <kid_1194@hotmail.com>
2022-07-15 19:23:43 +0300

    Update date js to v1.1.0

--- erpnext_fiscal_year_based_date_related_fields/public/js/erpnext_fiscal_year_based_date_related_fields.js
+++ erpnext_fiscal_year_based_date_related_fields/public/js/erpnext_fiscal_year_based_date_related_fields.js
@@ -1,10 +1,15 @@
 frappe.ui.form.ControlDate = class ControlDate extends frappe.ui.form.ControlDate {
+	set_input(value) {
+	    let dt = frappe.ui.form.respectFiscalYear(this, new Date(value));
+		value = moment(dt).format(moment(value).creationData().format);
+		super.set_input(value);
+	}
 	set_datepicker() {
 	    super.set_datepicker();
-	    frappe.ui.form.addFiscalYearSupport(this);
+	    frappe.ui.form.supportFiscalYear(this);
 	}
 	get_now_date() {
-	    let dt = frappe.ui.form.updateFiscalYearDate(this, frappe.datetime.now_date(true));
+	    let dt = frappe.ui.form.respectFiscalYear(this, frappe.datetime.now_date(true));
 	    return frappe.datetime.convert_to_system_tz(dt, false).toDate();
 	}
 };
