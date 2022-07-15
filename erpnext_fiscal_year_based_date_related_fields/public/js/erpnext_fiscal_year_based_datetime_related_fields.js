973d3e5f953f1c1aef11cf72c31ee0d04abb0448
Amin Ahmed <kid_1194@hotmail.com>
2022-07-15 19:25:51 +0300

    Update datetime js to v1.1.0

--- erpnext_fiscal_year_based_date_related_fields/public/js/erpnext_fiscal_year_based_datetime_related_fields.js
+++ erpnext_fiscal_year_based_date_related_fields/public/js/erpnext_fiscal_year_based_datetime_related_fields.js
@@ -1,29 +1,23 @@
 frappe.ui.form.ControlDatetime = class ControlDatetime extends frappe.ui.form.ControlDatetime {
+	set_input(value) {
+	    if (this.respectedFY) {
+    	        let dt = this._prep_date(new Date(value));
+    		value = moment(dt).format(moment(value).creationData().format);
+	    }
+	    super.set_input(value);
+	}
 	set_datepicker() {
 	    super.set_datepicker();
-	    frappe.ui.form.addFiscalYearSupport(this);
-	}
-	set_formatted_input(value) {
-		if (this.timepicker_only) return;
-		if (!this.datepicker) return;
-		if (!value) {
-			this.datepicker.clear();
-			return;
-		} else if (value === "Today") {
-			value = this.get_now_date();
-		}
-		value = this.format_for_input(value);
-		this.$input && this.$input.val(value);
-		this.datepicker.selectDate(this._prep_date(frappe.datetime.user_to_obj(value)));
+	    frappe.ui.form.supportFiscalYear(this);
 	}
 	get_start_date() {
-		let value = frappe.datetime.convert_to_user_tz(this.value);
-		return this._prep_date(frappe.datetime.str_to_obj(value));
+	    let value = frappe.datetime.convert_to_user_tz(this.value);
+	    return this._prep_date(frappe.datetime.str_to_obj(value));
 	}
 	get_now_date() {
 	    return this._prep_date(frappe.datetime.now_datetime(true));
 	}
 	_prep_date(dt) {
-	    return frappe.ui.form.updateFiscalYearDate(this, dt);
+	    return frappe.ui.form.respectFiscalYear(this, dt);
 	}
 };
