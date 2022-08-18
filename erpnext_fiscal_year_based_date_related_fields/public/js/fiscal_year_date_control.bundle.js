ba125da8ff784136cccc4b1376737fb37071b235
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 22:48:24 +0300

    Update to v1.2.1

--- erpnext_fiscal_year_based_date_related_fields/public/js/fiscal_year_date_control.bundle.js
+++ erpnext_fiscal_year_based_date_related_fields/public/js/fiscal_year_date_control.bundle.js
@@ -2,23 +2,27 @@ import {
     respectFiscalYear,
     updateDatePicker,
     getDateTime
-} from './utils';
+} from './utils/utils.js';
 
 frappe.ui.form.ControlDate = class ControlDate extends frappe.ui.form.ControlDate {
-	constructor(opts) {
-	    respectFiscalYear(this);
-	    super(opts);
-	}
-	set_input(value) {
-	    value = getDateTime(this, value, true);
-		super.set_input(value);
-	}
-	set_datepicker() {
-	    super.set_datepicker();
-	    updateDatePicker(this);
-	}
-	get_now_date() {
-	    let dt = getDateTime(this, null, true, true);
-	    return frappe.datetime.convert_to_system_tz(dt, false).toDate();
-	}
+    constructor(opts) {
+        respectFiscalYear(this);
+        super(opts);
+    }
+    set_value(value, force_set_value=false) {
+        value = getDateTime(this, value, true);
+        return super.set_value(value, force_set_value);
+    }
+    set_input(value) {
+        value = getDateTime(this, value, true);
+        super.set_input(value);
+    }
+    set_datepicker() {
+        super.set_datepicker();
+        updateDatePicker(this);
+    }
+    get_now_date() {
+        let dt = getDateTime(this, null, true, true);
+        return frappe.datetime.convert_to_system_tz(dt, false).toDate();
+    }
 };
