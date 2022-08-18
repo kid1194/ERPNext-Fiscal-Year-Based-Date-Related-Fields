66fc27f6c6a2714d3ad158788f0e9a721029941e
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 22:50:43 +0300

    Update to v1.2.1

--- erpnext_fiscal_year_based_date_related_fields/public/js/fiscal_year_datetime_control.bundle.js
+++ erpnext_fiscal_year_based_date_related_fields/public/js/fiscal_year_datetime_control.bundle.js
@@ -1,23 +1,32 @@
 import {
+    respectFiscalYear,
     updateDatePicker,
     getDateTime
-} from './utils';
+} from './utils/utils.js';
 
 frappe.ui.form.ControlDatetime = class ControlDatetime extends frappe.ui.form.ControlDatetime {
-	set_input(value) {
-	    value = getDateTime(this, value);
-		super.set_input(value);
-	}
-	set_datepicker() {
-	    super.set_datepicker();
-	    updateDatePicker(this);
-	}
-	get_start_date() {
-	    this.value = this.value == null ? undefined : this.value;
-	    let value = frappe.datetime.convert_to_user_tz(this.value);
-	    return getDateTime(this, this.value, false, true);
-	}
-	get_now_date() {
-	    return getDateTime(this, null, false, true);
-	}
+    constructor(opts) {
+        respectFiscalYear(this);
+        super(opts);
+    }
+    set_value(value, force_set_value=false) {
+        value = getDateTime(this, value, true);
+        return super.set_value(value, force_set_value);
+    }
+    set_input(value) {
+        value = getDateTime(this, value);
+        super.set_input(value);
+    }
+    set_datepicker() {
+        super.set_datepicker();
+        updateDatePicker(this);
+    }
+    get_start_date() {
+        this.value = this.value == null ? undefined : this.value;
+        let value = frappe.datetime.convert_to_user_tz(this.value);
+        return getDateTime(this, this.value, false, true);
+    }
+    get_now_date() {
+        return getDateTime(this, null, false, true);
+    }
 };
